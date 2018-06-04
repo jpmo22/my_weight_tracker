a402b56ca0c9f1b8c8e1eecb9f31799f0800a123
jpmo22 <jpmo22@gmail.com>
2018-06-03 23:13:14 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:my_weight_tracker/model/weight_rec.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() => runApp(new MyApp());
+
+double _currentWeight = 90.3;
+List<WeightRec> _data;
+final DateFormat dateFormat = DateFormat('MM/dd/yyyy HH:mm:ss');
+WeightRecProvider provider = WeightRecProvider();
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context)  {
+
+    return new MaterialApp(
+      title: 'My Weight Tracker',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'My Weight Tracker'),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  initState() {
+    super.initState();
+    initDB();
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "demo.db");
+    await provider.open(path);
+    _data = await provider.get();
+    provider.close();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: new Scaffold(
+          appBar: new AppBar(
+            title: Text(widget.title),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: const Icon(Icons.track_changes),
+                ),
+                Tab(
+                  icon: const Icon(Icons.history),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Scaffold(
+                body: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'kg',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      Text(
+                        _currentWeight.floor().toString(),
+                        style: TextStyle(
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '.' + ((_currentWeight - _currentWeight.floor())*10).toInt().toString(),
+                        style: TextStyle(fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return NumberPickerDialog.decimal(
+                          minValue: 0,
+                          maxValue: 200,
+                          initialDoubleValue: _currentWeight,
+                          title: Text('Enter your weight'),
+                        );
+                      },
+                    ).then((value) {
+                      print('New weight added... $value kg');
+                      if (value != null) {
+                        setState(() {
+                          _currentWeight = value;
+                          //_weightAdded(context, value);
+                        });
+                      }
+                    });
+                  },
+                  tooltip: 'Add new Weight entry',
+                  child: Icon(Icons.add),
+                ),
+              ),
+              ListView.builder(
+                itemBuilder: (BuildContext context, int index) =>
+                    HistoryWidget(_data[index]),
+                itemCount: _data?.length,
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class HistoryWidget extends StatelessWidget {
+  
+  final WeightRec rec;
+
+  HistoryWidget(this.rec);
+
+  @override
+  Widget build(BuildContext context) {
+    bool loss = rec.diff <= 0;
+    CircleAvatar icon = CircleAvatar(
+      backgroundColor: (loss ? Colors.green : Colors.red),
+      foregroundColor: Colors.white,
+      child: Text((loss ? '' : '+') + rec.diff.toString()),
+    );
+    return ListTile(
+      leading: icon,
+      title: Text(
+        rec.weight.toString(),
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      subtitle: Text(dateFormat.format(rec.dateTime)),
+    );
+  }
+}
+
---
+++ lib/model/weight_rec.dart
@@ -0,0 +1,121 @@
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:intl/intl.dart';
+
+const String _TABLE = 'Weights';
+const String _COL_ID = '_id';
+const String _COL_DATETIME = 'DateTime';
+const String _COL_WEIGHT = 'WeightValue';
+const String _COL_DIFF = 'Diff';
+
+final DateFormat dateFormat = DateFormat('yyyy-MM-ddTHH:mm:ss');
+
+class WeightRec {
+  int _id;
+  DateTime _dateTime;
+  double _weight;
+  double _diff;
+
+  int get id => _id;
+  DateTime get dateTime => _dateTime;
+  double get weight => _weight;
+  double get diff => _diff;
+  bool get isLoss => _diff <= 0;
+
+  WeightRec(this._id, this._dateTime, this._weight, this._diff);
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      _COL_ID: this._id,
+      _COL_DATETIME: dateFormat.format(this._dateTime),
+      _COL_WEIGHT: this._weight,
+      _COL_DIFF: this._diff
+    };
+    return map;
+  }
+
+  WeightRec.fromMap(Map map) {
+    _id = map[_COL_ID];
+    _dateTime = DateTime.parse(map[_COL_DATETIME]);
+    _weight = map[_COL_WEIGHT];
+    _diff = map[_COL_DIFF];
+  }
+}
+
+class WeightRecProvider {
+  Database db;
+
+  Future open(String path) async {
+    db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''CREATE TABLE $_TABLE (
+          $_COL_ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+          $_COL_DATETIME TEXT NOT NULL,
+          $_COL_WEIGHT REAL NOT NULL,
+          $_COL_DIFF REAL NOT NULL)''');
+    });
+  }
+
+  Future<WeightRec> insert(WeightRec weightRec) async {
+    int newId = await db.insert(_TABLE, weightRec.toMap());
+    return WeightRec(newId, weightRec._dateTime, weightRec._weight, weightRec.diff);
+  }
+
+  Future<List<WeightRec>> get() async {
+    List<Map> maps = await db.query(
+      _TABLE,
+      columns: [_COL_ID, _COL_DATETIME, _COL_WEIGHT, _COL_DIFF],
+    );
+
+    if (maps.length == 0){
+      sampleData();
+      return await get();
+    }
+
+    List<WeightRec> result = [];
+    for (Map m in maps){
+      result.add( WeightRec.fromMap(m) );
+    }
+    return result;
+  }
+
+  Future<WeightRec> getById(int id) async {
+    List<Map> maps = await db.query(
+      _TABLE,
+      columns: [_COL_ID, _COL_DATETIME, _COL_WEIGHT, _COL_DIFF],
+      where: '$_COL_ID = ?',
+      whereArgs: [id],
+    );
+    if (maps.length > 0) {
+      return WeightRec.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  Future<int> delete(int id) async {
+    return await db.delete(_TABLE, where: '$_COL_ID = ?', whereArgs: [id]);
+  }
+
+  Future<int> update(WeightRec weightRec) async {
+    return await db.update(_TABLE, weightRec.toMap(),
+        where: '$_COL_ID = ?', whereArgs: [weightRec._id]);
+  }
+
+  Future close() async => db.close();
+
+  void sampleData() {
+    insert(WeightRec(null, DateTime.parse('2018-05-26T22:30:25Z'), 98.1, -0.7));
+    insert(WeightRec(null, DateTime.parse('2018-05-26T22:30:25Z'), 98.1, -0.7));
+    insert(WeightRec(null, DateTime.parse('2018-05-26T22:30:25Z'), 98.1, -0.7));
+    insert(WeightRec(null, DateTime.parse('2018-05-25T21:58:10Z'), 98.8, 0.1));
+    insert(WeightRec(null, DateTime.parse('2018-05-24T22:12:45Z'), 98.7, -0.6));
+    insert(WeightRec(null, DateTime.parse('2018-05-23T22:22:23Z'), 99.3, -0.5));
+    insert(WeightRec(null, DateTime.parse('2018-05-22T23:01:43Z'), 99.8, -0.2));
+    insert(WeightRec(null, DateTime.parse('2018-05-21T22:13:10Z'), 100.0, -1.2));
+    insert(WeightRec(null, DateTime.parse('2018-05-20T20:45:09Z'), 101.2, 0.1));
+    insert(WeightRec(null, DateTime.parse('2018-05-19T19:32:05Z'), 101.1, -0.6));
+    insert(WeightRec(null, DateTime.parse('2018-05-18T23:10:13Z'), 101.7, 0.0));
+  }
+}
+
+
